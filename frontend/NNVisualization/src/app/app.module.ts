a5c41947019d1ba46e4456fe5ca94d97a28c89fb
gasiort <gasior.tt@gmail.com>
2017-12-28 12:11:29 +0100

    Add header and navbar component

--- frontend/NNVisualization/src/app/app.module.ts
+++ frontend/NNVisualization/src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
