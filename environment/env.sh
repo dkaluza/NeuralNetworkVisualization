79a1a6b73a78a77951fbf1e43922dd464bfe83e1
dzjkb <jakub.pies@gmail.com>
2017-12-30 12:17:04 +0100

    docker stuffs

--- environment/env.sh
+++ environment/env.sh
@@ -1,32 +1,34 @@
 #!/bin/bash
 
+ROOTDIR=$(git rev-parse --show-toplevel)
+
 do-build()
 {
-	docker build -t nnvis "environment"
+    docker build -t nnvis "$ROOTDIR/environment"
 }
 
 do-run()
 {
-	if [ $(docker ps | grep nnvis-container) -eq 0 ]; then
-		docker run --name nnvis-container -it -d -p 80:80 nnvis
-	fi
-}
-
-do-connect()
-{
-	docker attach nnvis-container
+    if [[ "$(docker ps | grep nnvis-container)" -eq 0 ]]; then
+        docker run \
+               -v $ROOTDIR:/shared \
+               --rm=true \
+               --name nnvis-container \
+               -it \
+               -p 80:80 \
+               nnvis
+    fi
 }
 
 main()
 {
-	if [ "$#" -ne 0 ]; then
-		echo "No args pls"
-		exit 1
-	fi
+    if [ "$#" -ne 0 ]; then
+        echo "No args pls"
+        exit 1
+    fi
 
-	do-build
-	do-run
-	do-connect
+    do-build
+    do-run
 }
 
 main "$@"
