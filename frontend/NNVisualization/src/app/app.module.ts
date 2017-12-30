4d2da37d791c737c1ab4e93598961cadbe6fd7d5
gasiort <gasior.tt@gmail.com>
2017-12-30 12:58:03 +0100

    add routing

--- frontend/NNVisualization/src/app/app.module.ts
+++ frontend/NNVisualization/src/app/app.module.ts
@@ -5,16 +5,35 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { ManageComponent } from './manage/manage.component';
+import { BuildComponent } from './build/build.component';
+import { TrainComponent } from './train/train.component';
+import { VisualizeComponent } from './visualize/visualize.component';
+import {RouterModule, Routes} from '@angular/router';
+import {FormsModule} from '@angular/forms';
 
+
+const appRoutes: Routes = [
+  { path: '', component: ManageComponent},
+  { path: 'build', component: BuildComponent },
+  { path: 'train', component: TrainComponent},
+  { path: 'visualize', component: VisualizeComponent},
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    NavbarComponent
+    NavbarComponent,
+    ManageComponent,
+    BuildComponent,
+    TrainComponent,
+    VisualizeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
