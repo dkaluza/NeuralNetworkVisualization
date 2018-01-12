68556cfeec5dce24bcc63b77c73adb9de348573d
Adam Jędrych <adam.jedrych25@gmail.com>
2018-01-12 18:36:49 +0100

    Implemented tables for archs and models

--- frontend/NNVisualization/src/app/app.module.ts
+++ frontend/NNVisualization/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -9,17 +10,50 @@ import { ManageComponent } from './manage/manage.component';
 import { BuildComponent } from './build/build.component';
 import { TrainComponent } from './train/train.component';
 import { VisualizeComponent } from './visualize/visualize.component';
-import {RouterModule, Routes} from '@angular/router';
-import {FormsModule} from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RestangularModule, Restangular } from 'ngx-restangular';
-
+import {
+    MatAutocompleteModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatChipsModule,
+    MatDatepickerModule,
+    MatDialogModule,
+    MatExpansionModule,
+    MatGridListModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    MatMenuModule,
+    MatNativeDateModule,
+    MatPaginatorModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
+    MatRadioModule,
+    MatRippleModule,
+    MatSelectModule,
+    MatSidenavModule,
+    MatSliderModule,
+    MatSlideToggleModule,
+    MatSnackBarModule,
+    MatSortModule,
+    MatTableModule,
+    MatTabsModule,
+    MatToolbarModule,
+    MatTooltipModule,
+    MatStepperModule,
+} from '@angular/material';
+import {CdkTableModule} from '@angular/cdk/table';
 
 const appRoutes: Routes = [
-  { path: '', redirectTo: 'manage', pathMatch: 'full'},
-  { path: 'manage', component: ManageComponent},
-  { path: 'build', component: BuildComponent},
-  { path: 'train', component: TrainComponent},
-  { path: 'visualize', component: VisualizeComponent},
+    { path: '', redirectTo: 'manage', pathMatch: 'full'},
+    { path: 'manage', component: ManageComponent},
+    { path: 'build', component: BuildComponent},
+    { path: 'train', component: TrainComponent},
+    { path: 'visualize', component: VisualizeComponent},
 ];
 
 // Function for setting the default restangular configuration
@@ -29,22 +63,66 @@ export function RestangularConfigFactory (RestangularProvider) {
 }
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    NavbarComponent,
-    ManageComponent,
-    BuildComponent,
-    TrainComponent,
-    VisualizeComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    RouterModule.forRoot(appRoutes),
-    RestangularModule.forRoot(RestangularConfigFactory)
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    exports: [
+        CdkTableModule,
+        MatAutocompleteModule,
+        MatButtonModule,
+        MatButtonToggleModule,
+        MatCardModule,
+        MatCheckboxModule,
+        MatChipsModule,
+        MatStepperModule,
+        MatDatepickerModule,
+        MatDialogModule,
+        MatExpansionModule,
+        MatGridListModule,
+        MatIconModule,
+        MatInputModule,
+        MatListModule,
+        MatMenuModule,
+        MatNativeDateModule,
+        MatPaginatorModule,
+        MatProgressBarModule,
+        MatProgressSpinnerModule,
+        MatRadioModule,
+        MatRippleModule,
+        MatSelectModule,
+        MatSidenavModule,
+        MatSliderModule,
+        MatSlideToggleModule,
+        MatSnackBarModule,
+        MatSortModule,
+        MatTableModule,
+        MatTabsModule,
+        MatToolbarModule,
+        MatTooltipModule,
+    ]
+})
+export class MaterialImportsModule {}
+
+
+@NgModule({
+    declarations: [
+        AppComponent,
+        HeaderComponent,
+        NavbarComponent,
+        ManageComponent,
+        BuildComponent,
+        TrainComponent,
+        VisualizeComponent,
+    ],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        FormsModule,
+        ReactiveFormsModule,
+        RouterModule.forRoot(appRoutes),
+        RestangularModule.forRoot(RestangularConfigFactory),
+        MaterialImportsModule,
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+platformBrowserDynamic().bootstrapModule(AppModule);
--- frontend/NNVisualization/src/app/manage/manage.component.ts
+++ frontend/NNVisualization/src/app/manage/manage.component.ts
@@ -1,5 +1,22 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { Restangular } from 'ngx-restangular';
+import { MatTableDataSource } from '@angular/material';
+
+interface Element {
+    position: number;
+    name: string;
+    id: number;
+}
+
+interface Arch {
+    name: string;
+    id: number;
+}
+
+interface Model {
+    name: string;
+    id: number;
+}
 
 @Component({
     selector: 'app-manage',
@@ -8,9 +25,16 @@ import { Restangular } from 'ngx-restangular';
 })
 export class ManageComponent implements OnInit {
 
-    architectures: string[] = [];
-    models: string[] = [];
-    popup: boolean = false
+    architectures: Arch[] = [];
+    models: Model[] = [];
+    add_new_arch_mode = false;
+
+    new_arch_name = '';
+    new_arch_desc = '';
+
+    displayedColumns = ['position', 'name'];
+    archDataSource = new MatTableDataSource<Element>([]);
+    modelDataSource = new MatTableDataSource<Element>([]);
 
     constructor(private restangular: Restangular) {
     }
@@ -19,32 +43,53 @@ export class ManageComponent implements OnInit {
         this.restangular.all('listarchs')
             .getList().subscribe(_architectures => {
                 this.architectures = _architectures;
+
+                const archElems = [];
+                for (let i = 0; i < this.architectures.length; i += 1) {
+                    archElems.push({
+                        position: i + 1,
+                        name: this.architectures[i].name,
+                        id: this.architectures[i].id,
+                    });
+                }
+                this.archDataSource = new MatTableDataSource<Element>(archElems);
             });
     }
 
-    addNewArchitecture() {
-        this.popup = true
+    applyFilter(dataSource, filterValue: string) {
+        filterValue = filterValue.trim();
+        filterValue = filterValue.toLowerCase();
+        dataSource.filter = filterValue;
     }
 
-    saveNewArchitecture(n, d) {
-        if (n == "" || d == "") {
-            return;
-        }
-        console.log("Name: " + n +
-            " , Description: " + d);
-        this.popup = false;
+    addNewArchitecture() {
+        this.add_new_arch_mode = true;
+    }
+
+    saveNewArchitecture() {
+        this.add_new_arch_mode = false;
     }
 
     selectArchitecture(id) {
-        console.log("You have selected architecture: " + id);
+        console.log('You have selected architecture: ' + id);
         this.restangular.one('listmodels', id)
             .getList().subscribe(_models => {
                 this.models = _models;
+
+                const modelElems = [];
+                for (let i = 0; i < this.models.length; i += 1) {
+                    modelElems.push({
+                        position: i + 1,
+                        name: this.models[i].name,
+                        id: this.models[i].id,
+                    });
+                }
+                this.modelDataSource = new MatTableDataSource<Element>(modelElems);
             });
     }
 
     selectModel(id) {
         // TODO: selecting model
-        console.log("You have selected model: " + id);
+        console.log('You have selected model: ' + id);
     }
 }
