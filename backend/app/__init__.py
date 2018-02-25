b190e12866f966ae24f1043be72c18c87d0c8972
Adam Jędrych <adam.jedrych25@gmail.com>
2018-02-25 13:59:09 +0100

    fixes constant name in create_app

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -7,7 +7,7 @@ from flask_cors import CORS
 def create_app(config_filename):
     app = Flask(__name__)
     CORS(app)
-    app.config.from_object('config')
+    app.config.from_object(config_filename)
 
     db.init_app(app)
     with app.app_context():
