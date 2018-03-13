8eddfc92b01f1af1b4097e027d0f55823b96f4a8
Adam Jędrych <adam.jedrych25@gmail.com>
2018-03-13 21:01:51 +0100

    removed unnecessary lines

--- frontend/NNVisualization/src/app/selected-architecture/selected-architecture.service.ts
+++ frontend/NNVisualization/src/app/selected-architecture/selected-architecture.service.ts
@@ -40,9 +40,6 @@ export class SelectedArchitectureService {
                 }
             );
             this._currentLinks = this._architecture.links;
-        } else {
-            this._currentNodes = new Map;
-            this._currentLinks = [];
         }
     }
 
