9ce9ab946f85f69a6bdf53f36341b5998e5a4ade
gasiort <gasior.tt@gmail.com>
2018-03-19 17:39:38 +0100

    vis utils

---
+++ backend/app/vis_tools/visualize_utils.py
@@ -0,0 +1,63 @@
+import tensorflow as tf
+import numpy as np
+import cv2
+
+mocked_alg_hooks = {
+    0: GradientSaliency,
+    1: GuidedBackprop
+}
+
+def preprocess(img):
+    img /= 255.0
+    return img
+
+def postprocess(img):
+    img -= img.min()
+    img /= img.max()
+    img *= 255
+    img = img.astype(np.uint8)
+    return img
+
+def load_image(image_path, proc=False):
+    image = cv2.imread(image_path)
+    if proc:
+        image = preprocess(image)
+    return image
+
+def save_image(image, image_path, proc=False):
+    if proc:
+        image = postprocess(image)
+    cv2.imwrite(image_path, image)
+
+# mocked for now
+def load_model(model):
+    graph = tf.Graph()
+    with graph.as_default():
+        # mocked model
+        saver = tf.train.import_meta_graph('mockup/models/mock_cnn_cifar10.ckpt.meta')
+        sess = tf.Session(graph=graph)
+        saver.restore(sess, tf.train.latest_checkpoint('mockup/models/'))
+
+        # important !!! - logits tensor and image placeholder need to be named with agreed convention
+        logits = graph.get_tensor_by_name('mock_cnn/fc5/logits:0')
+        x = graph.get_tensor_by_name('x:0')
+
+        neuron_selector = tf.placeholder(tf.int32)
+        y = logits[0][neuron_selector]
+
+        # Construct tensor for predictions.
+        # prediction = tf.argmax(logits, 1)
+
+    return graph, sess, x, y, neuron_selector, logits
+
+
+def softmax(x):
+    e_x = np.exp(x - np.max(x))
+    return e_x / e_x.sum()
+
+def inference(sess, logits, x, image_input):
+    logits_score = logits.eval(feed_dict={x: [image_input]}, session=sess)
+    predictions = softmax(logits_score[0])
+    return predictions
+
+
