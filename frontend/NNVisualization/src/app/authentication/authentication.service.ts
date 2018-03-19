0f06fb5670252f844c8f1c68df200f365ebd06b7
dkaluza <daniel.kaluza.dk@gmail.com>
2018-03-19 12:39:23 +0100

    Header styling (#65)
    * Remove bootstrap
    
    * Upgrade angular-jwt for angular 5 compatibility
    
    * Fix styling after bootstrap remove, merge headers
    
    * Add selected bar styling
    
    * Change toolbar styling :(

--- frontend/NNVisualization/src/app/authentication/authentication.service.ts
+++ frontend/NNVisualization/src/app/authentication/authentication.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { JwtHelper } from 'angular2-jwt';
+import { JwtHelperService as JwtHelper } from '@auth0/angular-jwt';
 import { Restangular } from 'ngx-restangular';
 import { Observable } from 'rxjs/Observable';
 
