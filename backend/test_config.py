1ec2b6e6eea23e1bb9a66e80a7ed5f290267edb0
dkaluza <daniel.kaluza.dk@gmail.com>
2018-03-19 15:12:05 +0100

    Various compatibility fixes (#72)
    * Delete idea settings from repo
    
    * Move tests temp and datasets to app folder
    
    * Change datasets directory
    
    * Add missing /

--- backend/test_config.py
+++ backend/test_config.py
@@ -8,6 +8,6 @@ TEST_DB_FILENAME = 'test_models.db'
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + TEST_DB_FILENAME
 JWT_SECRET_KEY = 'super_secret_stuff_for_testing'
 
-TMP_FOLDER = os.path.join(os.getenv('HOME'), 'nnvis_test_tmp')
+TMP_FOLDER = os.path.join(os.path.dirname(__file__), 'app', 'nnvis_test_tmp')
 DATASET_FOLDER = os.path.join(TMP_FOLDER, 'datasets')
 LABELS_FILENAME = 'labels.csv'
