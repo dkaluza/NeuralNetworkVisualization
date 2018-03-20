bc50c7ca0f6c2d9e7d171e9553db7c655a4a9cfc
gasiort <gasior.tt@gmail.com>
2018-03-20 17:56:40 +0100

    change image model so now it resembles what we have on backend side

--- frontend/NNVisualization/src/app/visualize/image.model.ts
+++ frontend/NNVisualization/src/app/visualize/image.model.ts
@@ -1,11 +1,15 @@
 export class Image {
+    public datasetId: number;
     public imageId: number;
     public imageName: string;
     public imagePath: string;
+    public label: number;
 
-    constructor(imageId: number, imageName: string, imagePath: string) {
+    constructor(imageId: number, datasetId: number, imageName: string, imagePath: string, label: number) {
         this.imageId = imageId;
+        this.datasetId = datasetId;
         this.imageName = imageName;
         this.imagePath = imagePath;
+        this.label = label;
     }
 }
