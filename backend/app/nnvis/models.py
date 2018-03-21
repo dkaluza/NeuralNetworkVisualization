f98bdfb4237a949c5f8266de4786c6d3ea7f7920
Adam Jędrych <adam.jedrych25@gmail.com>
2018-03-21 13:17:47 +0100

    fix

--- backend/app/nnvis/models.py
+++ backend/app/nnvis/models.py
@@ -112,7 +112,7 @@ class Dataset(db.Model, CRUD):
     labels = db.Column(db.Text(256), nullable=False)
     models = db.relationship('Model', backref='dataset', lazy=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    images = db.relationship('Image', cascade='all, delete',
+    images = db.relationship('Image', cascade='all, delete-orphan',
                              backref='dataset', lazy=True)
 
     def __init__(self, name, description, path, labels, user_id):
