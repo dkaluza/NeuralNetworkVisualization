5a53f257dc5eba2910e031917eb8f8d061c19031
dkaluza <daniel.kaluza.dk@gmail.com>
2018-03-21 12:52:29 +0100

    Change time before automatic log out (#79)

--- backend/config.py
+++ backend/config.py
@@ -1,10 +1,12 @@
 import os
+from datetime import timedelta
 
 HOST = '127.0.0.1'
 PORT = 5000
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///models.db'
 JWT_SECRET_KEY = 'we_need_some_secret_key'
+JWT_ACCESS_TOKEN_EXPIRES = timedelta(days=1)
 
 WEIGHTS_DIR = os.path.join(os.path.dirname(__file__), 'app', 'weights')
 
