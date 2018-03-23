9c651a185a3b97873b288e225e935f3076bb7eb0
Adam Jędrych <adam.jedrych25@gmail.com>
2018-03-23 16:32:39 +0100

    Added tensor names

--- frontend/NNVisualization/src/app/vis-arch/layers/input/input.ts
+++ frontend/NNVisualization/src/app/vis-arch/layers/input/input.ts
@@ -2,21 +2,34 @@ import { Layer } from '../layer/layer';
 import { ArchNode } from '../../../selected-architecture/architecture';
 
 export class InputLayer extends Layer {
+    private _inputId: number;
 
     constructor(id: number, label: string,
-                input = '-1, 28, 28, 1', output = '-1, 28, 28, 1') {
+        input = '-1, 28, 28, 1', output = '-1, 28, 28, 1',
+        inputId = 1) {
         super(id, label, 'input', input, output);
+        this._inputId = inputId;
+    }
+
+    get inputId(): number {
+        return this._inputId;
+    }
+
+    set inputId(inputId: number ) {
+        this._inputId = inputId;
     }
 
     static fromDict(dict: ArchNode): InputLayer {
         return new InputLayer(
             Number(dict.id), dict.label,
             String(dict.params.inputShape),
-            String(dict.params.outputShape)
+            String(dict.params.outputShape),
+            Number(dict.params.inputId)
         );
     }
 
     addAttributes(dict) {
+        dict['inputId'] = this._inputId;
         return dict;
     }
 }
