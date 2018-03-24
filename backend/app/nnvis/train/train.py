c4c8da9df5685100fe9094951f9e3fc6a113b9b7
dzjkb <jakub.pies@gmail.com>
2018-03-24 10:03:59 +0100

    added context teardown

--- backend/app/nnvis/train/train.py
+++ backend/app/nnvis/train/train.py
@@ -99,39 +99,39 @@ class TrainThread(threading.Thread):
         return np.mean(batch_losses)
 
     def run(self):
-        self.app_ctx.push()
-        train_ids = get_train_ids(self._dataset_id)
-        train_ids, valid_ids = split_into_train_and_valid(train_ids, 0.7)
-        self.__build_model()
+        with self.app_ctx:
+            train_ids = get_train_ids(self._dataset_id)
+            train_ids, valid_ids = split_into_train_and_valid(train_ids, 0.7)
+            self.__build_model()
 
-        print('starting training')
-        start_time = time.time()
-        with self._tfmodel.get_graph().as_default():
-            saver = tf.train.Saver()
+            print('starting training')
+            start_time = time.time()
+            with self._tfmodel.get_graph().as_default():
+                saver = tf.train.Saver()
 
-            with tf.Session() as sess:
-                sess.run(tf.global_variables_initializer())
+                with tf.Session() as sess:
+                    sess.run(tf.global_variables_initializer())
 
-                for e in range(self._nepochs):
-                    start_epoch = time.time()
-                    print('---- Epoch {e} ----'.format(e=e))
-                    train_ids = shuffle(train_ids)
-                    average_loss = self.__runepoch(sess, train_ids, train=True)
-                    self._training_loss += average_loss
-                    end_epoch = time.time()
+                    for e in range(self._nepochs):
+                        start_epoch = time.time()
+                        print('---- Epoch {e} ----'.format(e=e))
+                        train_ids = shuffle(train_ids)
+                        average_loss = self.__runepoch(sess, train_ids, train=True)
+                        self._training_loss += average_loss
+                        end_epoch = time.time()
 
-                    print('[Epoch {e}] Avg. loss = {loss}'
-                          .format(e=e, loss=average_loss))
-                    print('[Epoch {e}] Time = {t}'
-                          .format(e=e, t=end_epoch-start_epoch))
-                print('finished training')
-                self._training_loss /= float(self._nepochs)
+                        print('[Epoch {e}] Avg. loss = {loss}'
+                              .format(e=e, loss=average_loss))
+                        print('[Epoch {e}] Time = {t}'
+                              .format(e=e, t=end_epoch-start_epoch))
+                    print('finished training')
+                    self._training_loss /= float(self._nepochs)
 
-                print('staring validation')
-                self._validation_loss = self.__runepoch(sess, valid_ids,
-                                                        train=False)
-                print('Validation loss = {loss}'.format(loss=average_loss))
-                print('finished validation')
-                self.__save_model(sess, saver)
-        end_time = time.time()
-        print('Model training time = {0}'.format(end_time - start_time))
+                    print('staring validation')
+                    self._validation_loss = self.__runepoch(sess, valid_ids,
+                                                            train=False)
+                    print('Validation loss = {loss}'.format(loss=average_loss))
+                    print('finished validation')
+                    self.__save_model(sess, saver)
+            end_time = time.time()
+            print('Model training time = {0}'.format(end_time - start_time))
