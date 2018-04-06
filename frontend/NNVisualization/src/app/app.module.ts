1e56401627f85efad5bb0f0812f84351f3bd5c75
Tomasz <gasior.tt@gmail.com>
2018-04-06 13:52:00 +0200

    Issue 119 (#128)
    * fix angular material select
    
    * fix angular material select

--- frontend/NNVisualization/src/app/app.module.ts
+++ frontend/NNVisualization/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { NgSelectModule } from '@ng-select/ng-select';
 
 import { MaterialImportsModule } from './material-imports.module';
 
@@ -17,7 +18,7 @@ import { SelectedArchitectureService } from './selected-architecture/selected-ar
 import { VisualizeComponent } from './visualize/visualize.component';
 import { NavAlgorithmsComponent } from './visualize/nav-algorithms/nav-algorithms.component';
 
-import { BuildModule, BuildComponent } from './build/build.module';
+import { BuildComponent, BuildModule } from './build/build.module';
 
 import { RouterModule, Routes } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -26,7 +27,7 @@ import { RestangularModule } from 'ngx-restangular';
 import { LogInDialogComponent } from './header/log-in-dialog/log-in-dialog.component';
 import { AuthenticationGuardService as AuthGuard } from './authentication/authentication-guard.service';
 import { AuthenticationService, AuthenticationWithoutLoginService } from './authentication/authentication.service';
-import { JwtModule, JwtHelperService as JwtHelper } from '@auth0/angular-jwt';
+import { JwtHelperService as JwtHelper, JwtModule } from '@auth0/angular-jwt';
 import { UnauthorizedComponent } from './unauthorized/unauthorized.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { TimeoutAlertComponent } from './generic-dialogs/timeout-alert/timeout-alert.component';
@@ -132,7 +133,8 @@ export function RestangularConfigFactory(RestangularProvider,
                 }
             }
         }),
-        BuildModule
+        BuildModule,
+        NgSelectModule,
     ],
     providers: [
         SelectedArchitectureService,
