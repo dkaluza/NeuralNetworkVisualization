b4562528d9a110adde099b7bbb1b1804cc954b8f
gasiort <gasior.tt@gmail.com>
2018-04-11 01:34:10 +0200

    add list_postprocessing endpoint, change visualize endpoint

--- backend/app/nnvis/views.py
+++ backend/app/nnvis/views.py
@@ -4,17 +4,16 @@ from app import nnvis
 from app.nnvis.rests.architecture import (ArchitectureTask,
                                           UploadNewArchitecture,
                                           ListAllArchitectures)
-from app.nnvis.rests.model import (ModelTask,
-                                   UploadNewModel,
-                                   ListAllModels)
 from app.nnvis.rests.dataset import (DatasetTask,
                                      UploadNewDataset,
                                      ListAllDatasets)
-
-from app.nnvis.rests.visualize import (Inference, Visualize, Images,
-                                       ImageList, Algorithms)
+from app.nnvis.rests.model import (ModelTask,
+                                   UploadNewModel,
+                                   ListAllModels)
 from app.nnvis.rests.train import (TrainNewModel, TrainModel)
 from app.nnvis.rests.user import AuthenticationTask
+from app.nnvis.rests.visualize import (Inference, Visualize, Images,
+                                       ImageList, Algorithms, Postprocessing)
 
 api = Api(nnvis)
 
@@ -31,10 +30,11 @@ api.add_resource(UploadNewDataset, 'upload_dataset')
 api.add_resource(ListAllDatasets, 'list_datasets')
 
 api.add_resource(Inference, 'inference/<int:model_id>/<int:image_id>')
-api.add_resource(Visualize, 'visualize/<int:model_id>/<int:alg_id>/<int:image_id>')
+api.add_resource(Visualize, 'visualize/<int:model_id>/<int:alg_id>/<int:image_id>/<int:postprocessing_id>')
 api.add_resource(ImageList, 'images/<int:model_id>')
 api.add_resource(Images, 'image/<int:image_id>')
 api.add_resource(Algorithms, 'list_algorithms')
+api.add_resource(Postprocessing, 'list_postprocessing')
 
 api.add_resource(TrainNewModel, 'train_new_model/<int:arch_id>')
 api.add_resource(TrainModel, 'train_model/<int:model_id>')
